fixed minor bug in computation of fijab with regularization term nsyms on diagonal, not square of nsyms

src/mysca/core.py
@@ -186,7 +186,8 @@ def _compute_fijab_v1(xmsa, ws_norm, lam, nsyms, pbar=False, leave_pbar=False):
         ci = xmsa[:,i,:]
         for j in range(i, npos):
             cj = xmsa[:,j,:]
-            f = (1 - lam) * (ci.T @ (ws_norm[:, None] * cj)) + lam / nsyms**2
+            f = (1 - lam) * (ci.T @ (ws_norm[:, None] * cj)) \
+                + lam / (nsyms**2 * (i != j) + nsyms * (i == j))
             fijab[i,j,:,:] = f
             fijab[j,i,:,:] = f.T
     return fijab
@@ -198,14 +199,18 @@ def _compute_fijab_v2(xmsa, ws_norm, lam, nsyms, pbar=False, leave_pbar=False):
     fijab = np.full([npos, npos, naas, naas], np.nan)
 
     @jax.jit
-    def compute_f(ci, cj, ws_norm, lam, nsyms):
-        return (1 - lam) * (ci.T @ (ws_norm[:, None] * cj)) + lam / nsyms**2
+    def compute_f(ci, cj, ws_norm, lam, nsyms, regterm):
+        return (1 - lam) * (ci.T @ (ws_norm[:, None] * cj)) + regterm
     
     for i in tqdm.trange(npos, disable=not pbar, leave=leave_pbar):
         ci = xmsa[:,i,:]
         for j in range(i, npos):
             cj = xmsa[:,j,:]
-            f = compute_f(ci, cj, ws_norm, lam, nsyms)
+            if i == j:
+                regterm = np.array(lam / nsyms)
+            else:
+                regterm = np.array(lam / (nsyms * nsyms))
+            f = compute_f(ci, cj, ws_norm, lam, nsyms, regterm)
             fijab[i,j,:,:] = f
             fijab[j,i,:,:] = f.T
     return fijab

tests/test_core.py
@@ -226,7 +226,7 @@ def test_run_sca(
 
     errors = []
     for key in expected_results:
-        print(key)
+        print(key, np.array(expected_results[key]).shape, np.array(sca_res[key]).shape)
         if np.shape(expected_results[key]) != np.shape(sca_res[key]):
             msg = f"Shape mismatch in {key}\n"
             msg += f"Expected: {np.shape(expected_results[key])}\n"
@@ -234,7 +234,8 @@ def test_run_sca(
             errors.append(msg)
         if not np.allclose(expected_results[key], sca_res[key]):
             msg = f"Mismatch in {key}\n"
-            msg += f"Expected:\n{expected_results[key]}\n"
-            msg += f"     Got:\n{sca_res[key]}\n"
+            msg += f"{np.max(np.abs(np.array(expected_results[key]) - np.array(sca_res[key])))}"
+            # msg += f"Expected:\n{expected_results[key]}\n"
+            # msg += f"     Got:\n{sca_res[key]}\n"
             errors.append(msg)
     assert not errors, "Errors occurred:\n{}".format("\n".join(errors))